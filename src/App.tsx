bae8f84b2f054970d618e38c78b7df99fe53fe18
Michał Krzywicki <michal.krzywicki@binarapps.com>
2022-12-15 08:27:59 +0100

    UseState in action.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { TodoListItem } from "./TodoListItem";
 
 import { Todo } from './types';
 
-const todos: Todo[] = [
+const initialTodos: Todo[] = [
   {
     text: 'Wynieść śmieci',
     complete: true
@@ -15,9 +15,23 @@ const todos: Todo[] = [
 ];
 
 function App() {
+  const [todos, setTodos] = useState(initialTodos);
+
+  const toggleTodo = (selectedTodo: Todo) => {
+    const newTodos = todos.map((todo) => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  }
   return(
       <ul>
-          {todos.map((data) => <TodoListItem todo={data} />)}
+          {todos.map((data) => <TodoListItem todo={data} toggleTodo={toggleTodo} />)}
       </ul>
   );
 }
